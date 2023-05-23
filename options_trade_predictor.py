56fc54c9d58c6e6dd87d66d78b1c31112f5ba8e9
esamnyu <105534679+esamnyu@users.noreply.github.com>
2023-05-23 14:28:24 -0400

    Implemented Data Preprocessing and Model Training
    Added functionality to load trade data from an Excel file and preprocess it for machine learning. The preprocessing includes conversion of dates to numerical format and encoding of categorical variables. Also, split the data into training and test sets and standardize the features.
    
    Additionally, implemented the training of a simple binary classification neural network model using Keras. The model is trained to predict the success of a trade based on several features such as Entry Date, Exit Date, Ticker, Expiration Date, Strike Price, Opened Price, Stop Loss, Exit Price, and Trade Type.
    
    These changes provide the foundation for a machine learning approach to predicting trade outcomes

--- options_trade_predictor.py
+++ options_trade_predictor.py
@@ -1,17 +1,41 @@
-import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Assuming X_train and y_train are your training data,
-# where X_train is an array with each row being a different trade
-# characterized by features such as [entering price, exiting price, entering time, exiting time, entering volatility, exiting volatility, etc.]
-# and y_train is a binary array indicating whether each trade was good (1) or not (0)
+# Load your data
+df = pd.read_excel('your_file.xlsx')
 
+# Convert dates to datetime format and then to ordinal for numerical processing
+df['Entry_Date'] = pd.to_datetime(df['Entry_Date']).map(dt.datetime.toordinal)
+df['Exit_Date'] = pd.to_datetime(df['Exit_Date']).map(dt.datetime.toordinal)
+df['Expiration_Date'] = pd.to_datetime(df['Expiration_Date']).map(dt.datetime.toordinal)
+
+# Convert Ticker to numerical data using Label Encoding
+df['Ticker'] = df['Ticker'].astype('category')
+df['Ticker'] = df['Ticker'].cat.codes
+
+# Separate features and target
+X = df.drop(columns=['Outcome'])
+y = df['Outcome']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the data
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Define the model
 model = Sequential()
 model.add(Dense(32, input_dim=X_train.shape[1], activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))  # Sigmoid function for binary classification
 
+# Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Fit the model
 model.fit(X_train, y_train, epochs=50, batch_size=10)
